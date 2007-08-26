0cf5a68f29425892c8e5a048b153d7756bd3368e
Oliver Steele <steele@osteele.com>
2007-08-26 18:44:59 -0400

    add Function.K

--- functional.js
+++ functional.js
@@ -1,4 +1,7 @@
+/* Copyright 2007 by Oliver Steele.  All rights reserved. */
+
 Function.I = function(x) {return x};
+Function.K = function(x) {return function(){return x}};
 
 function forEach(fn, ar, thisObject) {
     return ar.forEach(fn, thisObject);
@@ -14,12 +17,14 @@ function filter(fn, ar, thisObject) {
 
 var select = filter;
 
+// Returns a projection this -> this[name]
 function pluck(name) {
     return function() {
         return this[name];
     }
 }
 
+// Returns a projection this -> this[name](args...)
 function invoke(name, args) {
     args = [].slice.call(args, 0);
     return function(object) {
