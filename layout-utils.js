3400342589333ab70ab03d851b5d06e4641506ed
Oliver Steele <steele@osteele.com>
2007-08-30 14:26:48 -0400

    new algorithm for adjacent position

--- layout-utils.js
+++ layout-utils.js
@@ -22,7 +22,7 @@ Size.prototype.maxSizeTo = function(maxWidth, maxHeight) {
  */
 
 // view must be within canvas
-function findBestRelativePosition(view, reference, container, margin) {
+function oldFindBestRelativePosition(view, reference, container, margin) {
     var refBounds = reference.getAbsoluteBounds(container),
         refLeft = refBounds.x,
         refTop = refBounds.y,
@@ -62,4 +62,61 @@ function findBestRelativePosition(view, reference, container, margin) {
         }
     }
     return {x:x, y:y};
+}
+
+function findBestRelativePosition(view, reference, container, margin) {
+    var refBounds = reference.getAbsoluteBounds(container),
+        refLeft = refBounds.x,
+        refTop = refBounds.y,
+        refRight = refBounds.x + refBounds.width,
+        refBottom = refBounds.y + refBounds.height;
+    var containerBounds = container.getAbsoluteBounds(),
+        containerLeft = containerBounds.x,
+        containerTop = containerBounds.y,
+        containerRight = containerBounds.x + containerBounds.width,
+        containerBottom = containerBounds.y + containerBounds.height;
+
+    // width, reference min/max, container min/max
+    // -> [[x, type, penalty]]
+    function positions(w, r0, r1, c0, c1, m) {
+        //info.apply(null,arguments);
+        // each entry is [position, type, weight]
+        // type is c=container | p=proximal | a=aligned
+        var cs = [
+            [c0+m, 'c', 0], [c1-m-w, 'c', 0],
+            [r0-m-w, 'p', -20], [r1+m, 'p', -10],
+            [r0, 'a', -5], [r1-w, 'a', -5]];
+        cs.each(function(item) {
+            var x = item[0], p = 0, w=5;
+            if (x < c0+m) p += w;
+            if (x < c0) p += w;
+            if (x+w > c1-m) p += w;
+            if (x+w < c1) p += w;
+            item[2] += p;
+        });
+        return cs;
+    }
+
+    var xs = positions(view.width, refLeft, refRight, containerLeft, containerRight, margin);
+    var ys = positions(view.height, refTop, refBottom, containerTop, containerBottom, margin);
+    var candidates = [];
+    xs.forEach(function(xe) {
+        ys.forEach(function(ye) {
+            var x = xe[0], y = ye[0];
+            var p = xe[2] + ye[2];
+            // penalize candidates that overlap the reference
+            if (x < refRight && x+view.width > refLeft && y < refBottom && y+view.height > refTop)
+                p += 100;
+            // penalize catty-corners
+            if (xe[1] == 'p' && ye[1] == 'p')
+                p += 500;
+            // if it's off the canvas, penalize it a lot
+            if (x < 0 || y < 0 || x+view.width > canvas.width || y+view.height > canvas.height)
+                p += 1000;
+            var b = {x: x, y: y, p: p};
+            candidates.push(b);
+        });
+    });
+    var best = candidates.sort(function(a,b){return a.p-b.p})[0];
+    return {x:best.x, y:best.y};
 }
