60be02963d6d59d83acbcef0a2429f333eb7bd05
Oliver Steele <steele@osteele.com>
2007-09-30 22:29:56 -0400

    simple-logging can log to browser console

--- simple-logging.js
+++ simple-logging.js
@@ -3,7 +3,7 @@
   Copyright: Copyright 2006 Oliver Steele.  All rights reserved.
   Download: http://osteele.com/sources/openlaszlo/simple-logging.js
   License: MIT License.
-  
+
   This file defines +info+, +warn+, +error+, and +debug+ functions
   that are compatible with those defined in
   {readable.js}[http://osteele.com/sources/javascript/readable.js],
@@ -21,3 +21,17 @@ function info() {__debug_message('info', arguments); return arguments[0]}
 function debug() {__debug_message('debug', arguments)}
 function warn() {Debug.warn.apply(Debug, arguments)}
 function error() {Debug.error.apply(Debug, arguments)}
+
+if (false) {
+    function info() {
+        var expr = ['window.console && console.info && console.info(',
+            JSON.stringify(arguments), ')'].join('');
+        LzBrowser.loadJS(expr);
+    }
+
+    function error() {
+        var expr = ['window.console && console.error && console.error(',
+            JSON.stringify(arguments), ')'].join('');
+        LzBrowser.loadJS(expr);
+    }
+}
