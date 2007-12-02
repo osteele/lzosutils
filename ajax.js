12ade368375f671d02fdcdd361d2b4491f5ff403
Oliver Steele <steele@osteele.com>
2007-12-02 09:31:52 -0500

    implement native POST

--- ajax.js
+++ ajax.js
@@ -11,20 +11,25 @@ function ajax(options) {
         return ajax.proxied(options);
     var url = options.url,
         onsuccess = options.success,
-        onerror = options.error;
-    if (options.data) {
-        var query = Hash.toQueryString(options.data);
-        if (query.length) {
-            if (url.indexOf('?') < 0) url += '?';
-            url += query;
+        onerror = options.error,
+        loader = new LoadVars,
+        post = options.type.toUpperCase()=='POST';
+    if (post) {
+        var sender = new LoadVars;
+        for (var name in options.data)
+            sender[name] = options.data[name];
+    } else {
+        if (options.data) {
+            var queryString = Hash.toQueryString(options.data);
+            if (queryString.length)
+                url = [url, url.indexOf('?') >= 0 ? '&' : '?', queryString
+                      ].join('');
         }
+        // add timestamp
+        if (!options.cache)
+            url = [url, url.indexOf('?') >= 0 ? '&' : '?',
+                   '_ts=', (new Date).getTime()].join('');
     }
-    ajax.trace && console.info('XHR', url);
-    // add timestamp
-    if (!options.cache)
-        url = [url, url.indexOf('?') >= 0 ? '&' : '?',
-               '_ts=', (new Date).getTime()].join('');
-    var loader = new LoadVars();
     loader.onLoad = function(success) {
         if (!success)
             onerror ? onerror() : console.error(url);
@@ -37,7 +42,10 @@ function ajax(options) {
             return onerror ? onerror() : console.error(url);
         onsuccess && onsuccess(json);
     };
-    loader.load(url);
+    ajax.trace && console.info(post ? 'POST' : 'GET', url);
+    post
+        ? sender.sendAndLoad(url, loader, 'POST')
+        : loader.load(url);
 }
 
 ajax.proxied = function(options) {
@@ -91,44 +99,11 @@ ajax.get = function(url, params, onsuccess, onerror) {
     ajax({url:url, data:params, success:onsuccess, error:onerror});
 }
 
-// when using LoadVars implementation does GET instead
 ajax.post = function(url, params, onsuccess, onerror) {
     if (typeof params == 'function') {
+        onerror = onsuccess;
+        onsuccess = params;
         params = {};
-        onsuccess = arguments[1];
-        onerror = arguments[2];
     }
     ajax({url:url, data:params, success:onsuccess, error:onerror, type:'POST'});
-}
-
-// Additional options:
-// - fallback: replacement options if the first call fails
-// - retries: retry this many times; a number or
-//            {count::Number, before::function, throttle::ms, backoff::%}
-// The retry +before+ function should return false to suppress the retry.
-// It is called with two arguments: the number of the current retry,
-// and a function that initiates the next retry.
-// (+before+ and +throttle+ are not implemented.)
-ajax.superGet = function(options) {
-    options = Hash.merge({}, options);
-    var onerror = options.error,
-        retries = options.retries;
-    if (retries)
-        options.retries = (retries instanceof Number
-                           ? {count:retries}
-                           : Hash.merge({}, retries));
-    options.error = function() {
-        var fallback = options.fallback,
-            retries = options.retries;
-        if (fallback) {
-            delete options.fallback;
-            Hash.merge(options, fallback);
-            ajax.superGet(options);
-        } else if (retries && retries.count >= 0) {
-            retries.count -= 1;
-            ajax.superGet.bind(null, options).defer(1000);
-        } else
-            onerror && onerror.apply(this, argument);
-    }
-    ajax(options);
 }
